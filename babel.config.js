e6d3d6d41796f3cd653ae163dc8ced695517c565
kubin51 <1562160184@qq.com>
2021-03-01 21:54:40 +0800

    init

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"]
+  presets: ["@vue/cli-plugin-babel/preset"],
+  // 配置ant-design-vue的按需加载，当style为true时表示加载less样式，相应的须开启vue.config.js中的javascripEnabled:true
+  plugins: [
+    [
+      "import",
+      { libraryName: "ant-design-vue", libraryDirectory: "es", style: true }
+    ]
+  ]
 };
